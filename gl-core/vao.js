5097d46e9c202996c330a0202380c87f47d34981
gnikoloff <nikoloffgeorgi@gmail.com>
2018-06-20 17:02:30 +0200

    even more vao stuff

--- gl-core/vao.js
+++ gl-core/vao.js
@@ -7,7 +7,7 @@ import { bindBuffer } from './bind-buffer'
 export class VAO {
     constructor (gl, attribs) {
         
-        this.vaoExtension = VAO.create(gl)
+        this.vaoExtension = getExtension(gl, `OES_vertex_array_object`)
         this.vao = this.vaoExtension.createVertexArrayOES()
 
         this.bind()
@@ -24,10 +24,6 @@ export class VAO {
 
         this.unbind()
         
-    }
-
-    static create (gl) {
-        return getExtension(gl, `OES_vertex_array_object`)
     }
 
     delete () {
