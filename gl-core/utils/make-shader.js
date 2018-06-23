6d6e22ef24699ff740b953eb2013bc9652b9abf8
gnikoloff <nikoloffgeorgi@gmail.com>
2018-06-23 16:31:41 +0200

    delete shader if there is compiling error

--- gl-core/utils/make-shader.js
+++ gl-core/utils/make-shader.js
@@ -10,13 +10,17 @@ export const makeShader = (gl, type, source) => {
     let shader = gl.createShader(type)
     gl.shaderSource(shader, source)
     gl.compileShader(shader)
+
     if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-        let shaderType = type === gl.VERTEX_SHADER ? 'vertex' : 'fragment'
+        let shaderType = type === gl.VERTEX_SHADER ? 'VERTEX' : 'FRAGMENT'
         console.error(
             `Error compiling ${shaderType} shader: 
             ${gl.getShaderInfoLog(shader)}`
         )
+        gl.deleteShader(shader)
+        
         return
     }
+
     return shader
 }
