d8211b5dfb9b5ecccb9659005c5f618761a85a70
gnikoloff <nikoloffgeorgi@gmail.com>
2018-06-23 11:34:17 +0200

    added drawing mode constants

--- gl-constants/index.js
+++ gl-constants/index.js
@@ -3,9 +3,16 @@ export const STATIC_DRAW = 0x88E4
 export const ELEMENT_ARRAY_BUFFER = 0x8893
 export const ARRAY_BUFFER = 0x8892
 
+// Draw modes
+export const POINTS = 0x0000
+export const LINES = 0x0001
+export const LINE_LOOP = 0x0002
+export const LINE_STRIP = 0x0003
 export const TRIANGLES = 0x0004
+export const TRIANGLE_STRIP = 0x0005
+export const TRIANGLE_FAN = 0x0006
 
-// texture constants
+// Texture constants
 export const RGB = 0x1907
 export const RGBA = 0x1908
 export const UNSIGNED_BYTE = 0x1401
